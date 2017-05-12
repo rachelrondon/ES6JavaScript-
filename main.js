309b158dba4faf3f3d9d300a4ab8e6b015d1e4cc
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-05-12 19:16:01 -0400

    update questions

--- main.js
+++ main.js
@@ -81,6 +81,7 @@ var speeds = trips.map(function(trip) {
 var speeds;
 
 // Question Four: Really Hard - Implementing 'Pluck'
+
 var paints = [ { color: 'red' }, { color: 'blue' }, { color: 'yellow' }];
 
 function pluck(array, property) {
@@ -94,3 +95,76 @@ function pluck(array, property) {
 }
 
 pluck(paints, 'color');
+
+
+// Filter method
+// Filter out or sort in a list
+// Return a filtered list of all of the items that have a type of vegetable
+
+var products = [
+  { name: 'cucumber', type: 'vegetable'},
+  { name: 'banana', type: 'fruit'},
+  { name: 'celery', type: 'vegetable'},
+  { name: 'orange', type: 'fruit'},
+];
+
+var filteredProducts = [];
+
+for (var i = 0; i < products.length; i++) {
+  if (products[i].type === 'fruit') {
+    filteredProducts.push(products[i]);
+  }
+}
+filteredProducts;
+
+
+products.filter(function(product) {
+  return product.type === 'vegetable';
+});
+
+// Another example:
+
+
+var products = [
+  { name: 'cucumber', type: 'vegetable', quantity: 0, price: 1 },
+  { name: 'banana', type: 'fruit', quantity: 10, price: 15 },
+  { name: 'celery', type: 'vegetable', quantity: 30, price: 13 },
+  { name: 'orange', type: 'fruit', quantity: 3, price: 5 },
+];
+
+// We are filtering the products where type is 'vegetable', quantity is greater than 0, price is less than 10
+products.filter(function(product) {
+//  return the products where type is equal to vegetable
+  return product.type === 'vegetable'
+// return the products where quantity is greater than zero
+  &&  product.quantity > 0
+// return the products where price is less than 10
+  && product.price < 10
+});
+
+
+var post = { id: 4, title: 'New Post' };
+var comments = [
+  { postId: 4, content: 'awesome post'},
+  { postId: 3, content: 'it was ok' },
+  { postId: 4, content: 'neat' }
+];
+
+function commentsForPost(post, comments) {
+  return comments.filter(function(comment) {
+    return comment.postId === post.id;
+  });
+}
+
+
+commentsForPost(post, comments);
+
+// Question Five: Filtering Values
+
+var numbers = [15, 25, 35, 45, 55, 65, 75, 85, 95];
+var filteredNumbers = [];
+
+numbers.filter(function(number) {
+   return number > 50;
+})
+var filteredNumbers;
